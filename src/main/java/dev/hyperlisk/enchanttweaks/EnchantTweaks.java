6bcc711e5cf6f361d5a93b63e085e1db13dd13ef
Carter Tomlenovich <cartertom@tuta.io>
2020-06-30 22:34:47 -0400

    wow, lambda time babeyy

--- src/main/java/dev/hyperlisk/enchanttweaks/EnchantTweaks.java
+++ src/main/java/dev/hyperlisk/enchanttweaks/EnchantTweaks.java
@@ -5,25 +5,21 @@ import dev.hyperlisk.enchanttweaks.blocks.XperiumBlock;
 import dev.hyperlisk.enchanttweaks.setup.ClientProxy;
 import dev.hyperlisk.enchanttweaks.setup.IProxy;
 import dev.hyperlisk.enchanttweaks.setup.ServerProxy;
+
+
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.jmx.Server;
 
 import java.util.stream.Collectors;
 
@@ -32,7 +28,7 @@ import java.util.stream.Collectors;
 public class EnchantTweaks
 {
 
-    public static IProxy proxy = DistExecutor.safeRunForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
+    public static IProxy proxy = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> ServerProxy::new);
 
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
@@ -60,7 +56,7 @@ public class EnchantTweaks
         }
 
         @SubscribeEvent
-        public static void onItemRegistry(final RegistryEvent.Register<Item> event) {
+        public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
 
             event.getRegistry().register(new BlockItem(ModBlocks.XPERIUMBLOCK, new Item.Properties()).setRegistryName("xperiumblock"));
         }
--- src/main/java/dev/hyperlisk/enchanttweaks/setup/ClientProxy.java
+++ src/main/java/dev/hyperlisk/enchanttweaks/setup/ClientProxy.java
@@ -4,8 +4,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.world.World;
 
 public class ClientProxy implements IProxy {
+
     @Override
     public World getClientWorld() {
         return Minecraft.getInstance().world;
     }
+
 }
