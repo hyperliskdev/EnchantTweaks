9f1856ecd2302e8d298b11bf66b16c121260eced
Carter Tomlenovich <cartertom@tuta.io>
2020-07-01 22:26:15 -0400

    basic block models and blockstates

--- src/main/java/dev/hyperlisk/enchanttweaks/EnchantTweaks.java
+++ src/main/java/dev/hyperlisk/enchanttweaks/EnchantTweaks.java
@@ -6,7 +6,6 @@ import dev.hyperlisk.enchanttweaks.setup.ClientProxy;
 import dev.hyperlisk.enchanttweaks.setup.IProxy;
 import dev.hyperlisk.enchanttweaks.setup.ServerProxy;
 
-
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -17,11 +16,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("enchanttweaks")
